881a53d5aa10b9b02856e68103c8e3838b1fdac8
Steven Cheng <a09930185@gmail.com>
2023-02-09 15:16:20 +0800

    green => create user

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,29 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserEntity } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
-class UserEntity {
-  email: string;
-  password: string;
-}
-
+const mockUserRepository = {
+  save: jest.fn(),
+  create: jest.fn(),
+};
 describe('AuthService', () => {
   let authService: AuthService;
 
+  let userRepository: Repository<UserEntity>;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
-
     authService = module.get<AuthService>(AuthService);
+    userRepository = module.get(getRepositoryToken(UserEntity));
   });
 
-  it('should be create user successful', () => {
+  it('should be create user successful', async () => {
     const email = 'sample@example.com';
     const password = 'password';
-    const user: UserEntity = {
-      email: email,
-      password: password,
-    };
-    expect(authService.createUser(email, password)).toEqual(user);
+    await authService.createUser(email, password);
+    expect(userRepository.create).toHaveBeenCalled();
+    expect(userRepository.save).toHaveBeenCalled();
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AuthService {
-  createUser(email: string, password: string) {
-    return undefined;
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+  async createUser(email: string, password: string): Promise<void> {
+    //crypt password
+    const genSalt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, genSalt);
+    //inject user repository
+    const user = await this.userRepository.create({
+      email: email,
+      password: hashedPassword,
+    });
+
+    //save into database
+    await this.userRepository.save(user);
   }
 }
