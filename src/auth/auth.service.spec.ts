1e5fad235c01a4820a8cf39751ecf10522c1ca6d
Steven Cheng <a09930185@gmail.com>
2023-02-09 16:38:35 +0800

    green => create datasource and use in memory test check save into database successful.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -2,35 +2,41 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UserEntity } from './user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
-const mockUserRepository = {
-  save: jest.fn(),
-  create: jest.fn(),
-};
 describe('AuthService', () => {
   let authService: AuthService;
-
   let userRepository: Repository<UserEntity>;
+  let datasource: DataSource;
+  beforeAll(async () => {
+    datasource = new DataSource({
+      type: 'sqlite',
+      database: ':memory:',
+      dropSchema: true,
+      entities: [UserEntity],
+      synchronize: true,
+    });
+    await datasource.initialize();
+    userRepository = datasource.getRepository(UserEntity);
+  });
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         AuthService,
-        {
-          provide: getRepositoryToken(UserEntity),
-          useValue: mockUserRepository,
-        },
+        { provide: getRepositoryToken(UserEntity), useValue: userRepository },
       ],
     }).compile();
+
     authService = module.get<AuthService>(AuthService);
-    userRepository = module.get(getRepositoryToken(UserEntity));
+  });
+  afterAll(async () => {
+    await datasource.destroy();
   });
 
   it('should be create user successful', async () => {
     const email = 'sample@example.com';
     const password = 'password';
     await authService.createUser(email, password);
-    expect(userRepository.create).toHaveBeenCalled();
-    expect(userRepository.save).toHaveBeenCalled();
+    expect(await userRepository.findOneBy({ email })).toMatchObject({ email });
   });
 });
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class UserEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
   @Column()
   email: string;
