54cc208fcce52f25935a5f7e3ad14eac78ee3613
Steven Cheng <a09930185@gmail.com>
2023-02-13 11:53:36 +0800

    green => verify email service should be successful send signUp email.

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -27,7 +27,12 @@ describe('EmailService', () => {
       const token = 'sampleToken';
       await mailService.sendVerification(email, token);
       expect(mailerService.sendMail).toHaveBeenCalledTimes(1);
-      expect(mailerService.sendMail).toHaveBeenCalledWith({});
+      expect(mailerService.sendMail).toHaveBeenCalledWith({
+        to: 'sample@example.com',
+        subject: 'Verify your email',
+        text: 'Please click this link http://localhost:3000/verify?token=sampleToken to verify your email.',
+        html: '<p>Please click <a href="http://localhost:3000/verify?token=sampleToken"> this link </a> to verify your email.</p>',
+      });
     });
   });
 });
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class MailService {
-  async sendVerification(email: string, token: string): Promise<any> {
-    return undefined;
+  constructor(private mailerService: MailerService) {}
+  async sendVerification(email: string, token: string): Promise<void> {
+    //TODO need to use configService and change localhost to base_url maybe http & 3000port also need to use.
+    const verifyUrl = `http://localhost:3000/verify?token=${token}`;
+    await this.mailerService.sendMail({
+      to: email,
+      subject: `Verify your email`,
+      text: `Please click this link ${verifyUrl} to verify your email.`,
+      html: `<p>Please click <a href="${verifyUrl}"> this link </a> to verify your email.</p>`,
+    });
   }
 }
